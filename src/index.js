31bf63a292c9c63752e7c77a1b100dd83a3eb80d
Andre Korkin <andre_korkin@mail.ru>
2021-11-18 19:34:44 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+let {square, cube, avg, digitsSum} = require('./math')
+let _ = require('lodash')
+
+
+// console.log(square(2), cube(3), avg([1,2,3,4,5]), digitsSum(123));
+console.log(_.chunk(['a', 'b', 'c', 'd'], 2))
---
+++ src/math.js
@@ -0,0 +1,29 @@
+function square(num) {
+	return num ** 2;
+}
+
+function cube(num) {
+	return num ** 3;
+}
+
+function avg(arr) {
+	return sum(arr, 1) / arr.length;
+}
+
+function digitsSum(num) {
+	return sum(String(num).split(''));
+}
+
+module.exports = {square, cube, avg, digitsSum}
+
+
+// вспомогательная функция
+function sum(arr) {
+	let res = 0;
+	
+	for (let elem of arr) {
+		res += +elem;
+	}
+	
+	return res;
+}
